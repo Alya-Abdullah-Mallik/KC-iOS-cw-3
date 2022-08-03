2d54c959a613314fe6742af8ec5cd15cb0a9ab60
Alya-Abdullah-Mallik <Mallik3014@gmail.com>
2022-08-03 18:39:40 +0300

    Info

---
+++ Get to Know Me.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var name:String = "Alya"
+var hobby:String = "reading"
+var favoriteNumber:Int = 6
+let BirthofDate:Int = 2006
+
+print("Hello , my name is \(name) and my favorite number is \(favoriteNumber) and my hobby is \(hobby) \n And I was born in \(BirthofDate) ")
